48ce2818c0bc25e8769421b63660e65d705c7624
Shvejan <shvejan20112gmail.com>
2023-05-05 16:45:49 -0400

    minio integrated

--- backend/app.py
+++ backend/app.py
@@ -5,17 +5,28 @@ import pandas as pd
 import duckdb
 import os
 import datamart_profiler
+from minio import Minio
+from io import BytesIO
 
 app = Flask(__name__)
 
 
 def download_and_save(id):
+    client = Minio(
+        "localhost:8050",
+        access_key="devkey",
+        secret_key="devpassword",
+        secure=False
+    )
     filename=id.replace(".","_").replace("-","_")+".parquet"
     response=requests.get('https://auctus.vida-nyu.org/api/v1/download/'+id)
     if response.status_code==200:
-        data=response.content.decode()
-        list=[x.split(',') for x in data.split('\n')]
-        df=pd.DataFrame(list[1:],columns=list[0])
+        try:
+            data=response.content.decode()
+            list=[x.split(',') for x in data.split('\n')]
+            df=pd.DataFrame(list[1:],columns=list[0])
+        except:
+            return False
 
         profiles=datamart_profiler.process_dataset(df)
         for c in profiles['columns']:
@@ -35,7 +46,18 @@ def download_and_save(id):
             except:
                 pass
 
-        df.to_parquet(filename)
+        parquet_buffer = BytesIO()
+        df.to_parquet(parquet_buffer)
+
+        # Upload the Parquet file to MinIO
+        parquet_buffer.seek(0)
+        client.put_object(
+            bucket_name="auctus-bucket",
+            object_name=filename,
+            data=parquet_buffer,
+            length=parquet_buffer.getbuffer().nbytes,
+            content_type="application/octet-stream"
+        )
         print("parquet file profiled and saved")
         
 
@@ -53,26 +75,50 @@ def query():
 
     filename=id.replace(".","_").replace("-","_")+".parquet"
 
-    if not os.path.isfile(filename):
+    client = Minio(
+        "localhost:8050",
+        access_key="devkey",
+        secret_key="devpassword",
+        secure=False
+    )
+
+    if not client.bucket_exists("auctus-bucket"):
+        client.make_bucket("auctus-bucket")
+
+    # bucket creation done 
+    try:
+        response = client.get_object("auctus-bucket", filename)
+        print("dtasaet found")
+    except:
         if not download_and_save(id):
             return {"data":"[]","error":True,"message":"Dataset is not downloadable","dtypes":"[]"}
+    
+
 
     con=duckdb.connect("flask.db")
+    con.sql("install 'httpfs';")
+    con.sql("load 'httpfs';")
+    con.sql("SET s3_url_style='path';")
+    con.sql(" SET s3_endpoint='localhost:8050'")
+    con.sql("SET s3_use_ssl=false;")
+    con.sql("SET s3_access_key_id='devkey';")
+    con.sql("SET s3_secret_access_key='devpassword';")
     query_result=[]
+
     if not query:
-        query_result=con.sql(" from "+filename)
+        query_result=con.sql(" select * from read_parquet('s3://auctus-bucket/{}');".format(filename))
 
     else:
         try:
-            query=query.replace("TABLE",filename)
+            query=query.replace("TABLE","read_parquet('s3://auctus-bucket/{}')".format(filename))
             query_result=con.sql(query)
         except:
             return {"data":"[]","error":True,"message":"Error in query","dtypes":"[]"}
         
     dtypes=query_result.dtypes
-    print(dtypes)
     query_result=query_result.fetchdf().to_json(orient='records')
     return {"data":query_result,"error":False,"message":"success","dtypes":dtypes}
+    # return {"data":"[]","error":True,"message":"Error in query","dtypes":"[]"}
 
 
 
